be51a1cf92174de2b64def952810cf632eafbf20
adamKonyari <adam.konyari@gmail.com>
2019-08-21 13:42:50 +0200

    NumberFormatException added for 400 status code

--- src/main/java/com/codecool/recipeapp/controllers/RecipeController.java
+++ src/main/java/com/codecool/recipeapp/controllers/RecipeController.java
@@ -1,11 +1,16 @@
 package com.codecool.recipeapp.controllers;
 
 import com.codecool.recipeapp.commands.RecipeCommand;
+import com.codecool.recipeapp.exceptions.NotFoundException;
 import com.codecool.recipeapp.services.RecipeService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
+@Slf4j
 @Controller
 public class RecipeController {
 
@@ -33,16 +38,39 @@ public class RecipeController {
         return "recipe/recipeform";
     }
 
-    @PostMapping("recipe") // @RequestMapping(name = "recipe", method = RequestMethod.POST) <= this is the older way of request mapping
+    @PostMapping("recipe")
+    // @RequestMapping(name = "recipe", method = RequestMethod.POST) <= this is the older way of request mapping
     public String saveOrUpdate(@ModelAttribute RecipeCommand recipeCommand) {
         RecipeCommand savedRecipeCommand = recipeService.saveRecipeCommand(recipeCommand);
         return "redirect:/recipe/" + savedRecipeCommand.getId() + "/show";
     }
 
     @GetMapping("recipe/{id}/delete")
-    public String deleteById(@PathVariable String id){
+    public String deleteById(@PathVariable String id) {
 
         recipeService.deleteById(Long.valueOf(id));
         return "redirect:/";
+    }
+
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(NotFoundException.class)
+    public ModelAndView handleNotFound(Exception exception) {
+        log.error("Handling not found exception.");
+        log.error(exception.getMessage());
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("404error");
+        modelAndView.addObject("exception",exception);
+        return modelAndView;
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(NumberFormatException.class)
+    public ModelAndView handleNumberFormatException(Exception exception) {
+        log.error("Handling number format exception.");
+        log.error(exception.getMessage());
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("400error");
+        modelAndView.addObject("exception", exception);
+        return modelAndView;
     }
 }
